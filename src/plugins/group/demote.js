8f6c90df71bbe92db7be0078c721ed6370756a13
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2025-10-28 23:46:46 +0530

    Update demote.js

--- src/plugins/group/demote.js
+++ src/plugins/group/demote.js
@@ -10,7 +10,7 @@ plugin(
         category: 'group',
         description: {
             content: 'Demote mentioned users from admin role.',
-            usage: '[mention user | quote user]',
+            usage: '<mention user | quote user>',
             example: '@917003213983'
         }
     },
