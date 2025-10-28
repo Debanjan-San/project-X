4db389579c8716ce13e0cc60f4d31bba454e8871
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2025-10-28 23:47:26 +0530

    Update remove.js

--- src/plugins/group/remove.js
+++ src/plugins/group/remove.js
@@ -10,7 +10,7 @@ plugin(
         category: 'group',
         description: {
             content: 'Remove mentioned or quoted users from the group.',
-            usage: '[mention user | quote user]',
+            usage: '<mention user | quote user>',
             example: '@917003213983'
         }
     },
